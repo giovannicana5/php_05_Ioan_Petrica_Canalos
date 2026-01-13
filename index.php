228dff96bd062cf00f42c3c66a0d11b81332454c
Ioan Petrica Canalos <giovanni.canalos@gmail.com>
2026-01-13 21:40:05 +0100

    Ho corretto l'errore che ho commesso nell'esercizio

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-echo ($numbers[1] + $numbers[3] + $numbers[5] + $numbers[7] + $numbers[9]) / 5;
+$counter = 0;
+$total = 0;
+foreach($numbers as $numeri) {
+    if($numeri % 2 == 0) {
+        $total += $numeri;
+        $counter++;
+    }
+}
+echo $total / $counter;
